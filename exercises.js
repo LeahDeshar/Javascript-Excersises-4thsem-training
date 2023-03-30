7c6489ad14bf21e3c831c332b44728b39490dadc
sabi <sabin@noveltytechnology.com>
2023-03-30 21:43:22 +0545

    first commit

---
+++ exercises.js
@@ -0,0 +1,76 @@
+// //1.  Remove the duplicate values from the array using filter method
+
+let arr = [1, 2, 3, 4, 4, 5, 6, 7, 8, 9, 7, 1];
+let students = [
+  {
+    id: 1,
+    name: "John",
+    age: 20,
+  },
+  {
+    id: 2,
+    name: "Doe",
+    age: 20,
+  },
+  {
+    id: 2,
+    name: "Doe",
+    age: 20,
+  },
+  {
+    id: 3,
+    name: "Harry",
+    age: 20,
+  },
+  {
+    id: 4,
+    name: "Marry",
+    age: 20,
+  },
+];
+
+// // Remove false,0,null,undefined, NaN,'' from the array;
+let array = [false, 0, NaN, 6, undefined, "", "Hi"];
+
+// // 2 . Get the initial from the string
+const input = "George Raymond Richard Martin";
+
+// // Result ="GRRM";
+
+// // 3. calculate the total number of votes from the given array
+var voters = [
+  { name: "Bob", age: 30, voted: true },
+  { name: "Jake", age: 32, voted: true },
+  { name: "Kate", age: 25, voted: false },
+  { name: "Sam", age: 20, voted: false },
+  { name: "Phil", age: 21, voted: true },
+  { name: "Ed", age: 55, voted: true },
+  { name: "Tami", age: 54, voted: true },
+  { name: "Mary", age: 31, voted: false },
+  { name: "Becky", age: 43, voted: false },
+  { name: "Joey", age: 41, voted: true },
+  { name: "Jeff", age: 30, voted: true },
+  { name: "Zack", age: 19, voted: false },
+];
+function totalVotes(arr) {
+  // your code here
+}
+
+// 4. convert the voters array into this format:
+var contacts = [
+  { name: "Bob", age: 30, voted: true },
+  { name: "Jake", age: 32, voted: true },
+  { name: "Kate", age: 25, voted: false },
+  { name: "Sam", age: 20, voted: false },
+  { name: "Phil", age: 21, voted: true },
+  { name: "Ed", age: 55, voted: true },
+  { name: "Babin", age: 55, voted: true },
+  { name: "Tami", age: 54, voted: true },
+  { name: "Telesko", age: 54, voted: true },
+  { name: "Mary", age: 31, voted: false },
+  { name: "Becky", age: 43, voted: false },
+  { name: "Joey", age: 41, voted: true },
+  { name: "Jeff", age: 30, voted: true },
+  { name: "Zack", age: 19, voted: false },
+  { name: "Zack", age: 19, voted: false },
+];
